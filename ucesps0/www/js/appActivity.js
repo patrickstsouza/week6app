3162793f011e3d05cf0b39e91e741a5d05981e3d
Patrick Souza <patrick.souza.17@ucl.ac.uk>
2018-03-11 15:33:23 -0400

    added ajax

--- ucesps0/www/js/appActivity.js
+++ ucesps0/www/js/appActivity.js
@@ -58,3 +58,27 @@ L.tileLayer(
             'Imagery © <a href="http://mapbox.com">Mapbox</a>',
         id: 'mapbox.streets'
     }).addTo(mymap);
+
+var xhr; // define the global variable to process the AJAX request
+function callDivChange() {
+    xhr = new XMLHttpRequest();
+    var filename = document.getElementById("filename").value;
+    xhr.open("GET", 'https://developer.cege.ucl.ac.uk:31078/' + filename, true);
+    xhr.onreadystatechange = processDivChange;
+    try {
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    }
+    catch (e) {
+        // this only works in internet explorer
+    }
+    xhr.send();
+}
+function processDivChange() {
+    if (xhr.readyState < 4) // while waiting response from server
+        document.getElementById('ajaxtest').innerHTML = "Loading...";
+    else if (xhr.readyState === 4) { // 4 = Response from server has been completely loaded.
+        if (xhr.status == 200 && xhr.status < 300)
+            // http status between 200 to 299 are all successful
+            document.getElementById('ajaxtest').innerHTML = xhr.responseText;
+    }
+}
